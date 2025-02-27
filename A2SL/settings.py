d1a1f94e498f3067e8fc71122c04a9f0bdbbde0f
Tamilarasan M <155798972+Tamilarasan-46@users.noreply.github.com>
2025-02-27 23:13:29 +0530

    Update settings.py

--- A2SL/settings.py
+++ A2SL/settings.py
@@ -29,7 +29,7 @@ if not SECRET_KEY:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
 #ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "audio-speech-to-sign-language.onrender.com").split(",")
 ALLOWED_HOSTS = ["audio-speech-to-sign-language.onrender.com"]
