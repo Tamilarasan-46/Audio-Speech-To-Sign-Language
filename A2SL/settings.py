3af217b4cc03d9e9441b5705b47a7adccdf9435f
Tamilarasan M <155798972+Tamilarasan-46@users.noreply.github.com>
2025-02-28 08:19:10 +0530

    Update settings.py

--- A2SL/settings.py
+++ A2SL/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 import nltk
 
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
